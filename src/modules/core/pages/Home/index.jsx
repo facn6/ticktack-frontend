eacdcbff558fa346d1074911860fa0243ccef951
Kira Ablamunits <kira.abl@gmail.com>
2019-10-20 00:19:08 +0300

    add footer with a logo

--- src/modules/core/pages/Home/index.jsx
+++ src/modules/core/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import Image from '@/modules/common/components/Image';
 import Slider from '@/modules/common/components/Slider';
 
+
 export default class Home extends PureComponent {
   render() {
     return (
