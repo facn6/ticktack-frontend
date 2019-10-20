5001ba1245afd84d0aa7ce478f4781f3457db439
ayah <ayah_kh5@hotmail.com>
2019-10-20 17:08:09 +0300

    add an artist page

--- src/modules/core/pages/Artist/index.jsx
+++ src/modules/core/pages/Artist/index.jsx
@@ -1,19 +1,15 @@
 import React, { PureComponent } from 'react';
-
 import Banner from '@/modules/common/components/Banner';
-import RotatingCube from '@/modules/common/components/RotatingCube';
 import Slider from '@/modules/common/components/Slider';
 import { withModal } from '@/modules/core/hoc/withModal';
 
 @withModal
- class Home extends PureComponent {
-
+ class Artist extends PureComponent {
     render() {
       console.log(this.props.modal);
         return (
             <div>
                 <Banner />
-
               <Slider />
                 <RotatingCube />
             </div>
@@ -21,4 +17,4 @@ import { withModal } from '@/modules/core/hoc/withModal';
     }
   }
 
-  export default Home;
+  export default Artist;
