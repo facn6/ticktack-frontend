d07fddefd93c9f591bad5672aa21905af64f16d8
DanaEFalah <dana.e.falah@gmail.com>
2019-10-20 11:32:31 +0300

    #3 add card to the home page

---
+++ src/modules/common/components/EventCard/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './style.scss';
+
+class EventCard extends React.Component {
+
+    render() {
+        return (
+            <div styleName="card">
+                <img src={require('@/assets/images/barca-real.jpg')} alt="event-img" styleName="event-img" />
+                <div styleName="burger-menu">
+                    <div styleName="burger-icon">
+                        <img src={require('@/assets/icons/circle.png')} alt="mashrom-icon" styleName="mashrom" />
+                        <img src={require('@/assets/icons/seen.svg')} alt="seen-icon" styleName="seen-icon" />
+                        <span styleName="view-res">12k</span>
+                        <div />
+                    </div>
+                </div>
+                <div styleName="card-content">
+                    <h1 styleName="title">שם של האירוע</h1>
+                    <p styleName="sub-title">תת כותרת</p>
+                    <div>
+                        <h4>מתאריך: </h4>
+                        <h4>06/06/2019</h4>
+                    </div>
+                    <div>
+                        <h4>עד</h4>
+                        <h4>09/06/2019</h4>
+                    </div>
+                </div>
+                <div styleName="card-price">
+                    <h4>החל מ-</h4>
+                    <p styleName="price">586$</p>
+                </div>
+
+            </div>
+        );
+    }
+
+}
+export default EventCard;
--- src/modules/core/pages/Home/index.jsx
+++ src/modules/core/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import Banner from '@/modules/common/components/Banner';
 import RotatingCube from '@/modules/common/components/RotatingCube';
+import EventCard from '@/modules/common/components/EventCard';
 
 export default class Home extends PureComponent {
 
@@ -9,6 +10,7 @@ export default class Home extends PureComponent {
             <div>
                 <Banner />
                 <RotatingCube />
+                <EventCard />
             </div>
         );
     }
