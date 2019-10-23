cc3ec3735907da2c351da6d53408bd4508718ae2
DanaEFalah <dana.e.falah@gmail.com>
2019-10-23 17:16:05 +0300

    resolve styles

--- src/modules/common/components/Footer/index.jsx
+++ src/modules/common/components/Footer/index.jsx
@@ -7,24 +7,24 @@ class Footer extends React.Component {
 
     render() {
         return (
-          <div styleName="field-div">
-          <div styleName="title">
+            <div styleName="field-div">
+                <div styleName="title">
           לקבלת דיוור על אירועים ומבצעים שווים
-          </div>
-          <div>
-          <input
-          styleName="field"
-            type="text"
-            id="input"
-            placeholder="הזן כתובת מייל"
-          />
-          </div>
-          <div>
-          <button styleName="send">שלח</button>
-          </div>
-          <img styleName="logo" src={require('@/assets/logos/logo-footer.svg')} alt="tick-tack-img" />
+                </div>
+                <div>
+                    <input
+                        styleName="field"
+                        type="text"
+                        id="input"
+                        placeholder="הזן כתובת מייל"
+                    />
+                </div>
+                <div>
+                    <button styleName="send">שלח</button>
+                </div>
+                <img styleName="logo" src={require('@/assets/logos/logo-footer.svg')} alt="tick-tack-img" />
 
-          </div>
+            </div>
         );
     }
 
