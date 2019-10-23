0a5518c16c4c9e6f2df25b3a522ae5ac10d56d21
ayah <ayah_kh5@hotmail.com>
2019-10-23 14:28:22 +0300

    add buttons for the events and style them

--- src/modules/common/components/EventsList/index.jsx
+++ src/modules/common/components/EventsList/index.jsx
@@ -8,11 +8,11 @@ class EventsList extends React.Component {
         return (
             <div styleName="list">
             <h1 styleName="title">כל האירועים של אן מארי</h1>
-      <ul >
-                      <li><button styleName="button">Coffee</button></li>
-                      <li><button styleName="button">Tea</button></li>
-                     <li><button styleName="button">Milk</button></li>
-      </ul>
+
+                    <button styleName="button"><span>פריז</span></button>
+                      <button styleName="button"><span>לונדון</span></button>
+
+
             </div>
         );
     }
