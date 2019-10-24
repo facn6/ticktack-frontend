15a8cfb81aa6cc92f15bf82c346fcb950552fc00
DanaEFalah <dana.e.falah@gmail.com>
2019-10-24 12:51:56 +0300

    #31 and #32 isuues - create detail container of events ticket price and location

--- src/modules/core/pages/Ticket/index.jsx
+++ src/modules/core/pages/Ticket/index.jsx
@@ -5,6 +5,9 @@ import NavSlider from '@/modules/common/components/NavSlider';
 import DetailsArtistContainer from '@/modules/common/components/DetailsArtistContainer';
 import ImageZoomer from '@/modules/common/components/ImageZoomer';
 import TicketDetails from '@/modules/common/components/TicketDetails';
+import TicketTypeList from '@/modules/common/components/TicketTypeList';
+import TicketDateList from '@/modules/common/components/TicketDateList';
+
 import './style.scss';
 
 export default class Ticket extends PureComponent {
@@ -17,7 +20,18 @@ export default class Ticket extends PureComponent {
                 <DetailsArtistContainer />
                 <p styleName="ticket-detail">פרטי כרטיסים</p>
                 <ImageZoomer />
-                <TicketDetails />
+                <TicketTypeList />
+                <TicketTypeList />
+                <TicketTypeList />
+                <TicketTypeList />
+                <p styleName="event-title">כל האירועים של אן מארי</p>
+                <div styleName="ticket-events">
+                    <TicketDateList />
+                    <TicketDateList />
+                    <TicketDateList />
+                    <TicketDetails />
+                </div>
+
             </div>
 
         );
