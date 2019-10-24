d3efe2d65f5dd8cdc6a4d1a0c90939b6f6d30a33
ayah <ayah_kh5@hotmail.com>
2019-10-24 14:15:29 +0300

    fix component

--- src/modules/common/components/Rectangle/index.jsx
+++ src/modules/common/components/Rectangle/index.jsx
@@ -8,6 +8,7 @@ export default class Rectangle extends React.Component {
                 <div styleName="city">{this.props.time}</div>
                 <div styleName="city">{this.props.location}</div>
             </div>
+
         );
     }
 }
