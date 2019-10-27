febd97e01968b395a113868deee4836ee570e569
Kira Ablamunits <kira.abl@gmail.com>
2019-10-27 19:46:37 +0200

    add a slider with navigation dots

--- src/modules/core/pages/Ticket/index.jsx
+++ src/modules/core/pages/Ticket/index.jsx
@@ -1,13 +1,17 @@
 import React, { PureComponent } from 'react';
 import Banner from '@/modules/common/components/Banner';
+import NavSlider from '@/modules/common/components/NavSlider';
+import './style.scss';
 
 export default class Ticket extends PureComponent {
-
     render() {
         return (
-            <Banner />
-
+            <div>
+                <div styleName="top-banner">
+                    <Banner />
+                </div>
+                <NavSlider />
+            </div>
         );
     }
-
 }
