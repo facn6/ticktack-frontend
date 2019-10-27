edf2cb5ffd8c676d91ae75f1b9f5d69753039ecb
DanaEFalah <dana.e.falah@gmail.com>
2019-10-27 21:46:58 +0200

    events page

--- src/modules/core/pages/Home/index.jsx
+++ src/modules/core/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
-
+import { asyncConnect } from 'redux-connect';
+import { connect } from 'quick-redux';
+import api from '@/helpers/api';
 import BannerWithButton from '@/modules/common/components/BannerWithButton';
 import RotatingCube from '@/modules/common/components/RotatingCube';
 import EventCard from '@/modules/common/components/EventCard';
@@ -8,9 +10,39 @@ import Banner from '@/modules/common/components/Banner';
 import Slider from '@/modules/common/components/Slider';
 import './style.scss';
 
-export default class Home extends PureComponent {
+@asyncConnect([
+    {
+        key: 'tickets',
+        promise: async () => {
+            const res = await api.fetchTickets();
+            console.log('res ', res);
+
+            if (res.fail) {
+                return [];
+            }
+            return res;
+        },
+    },
+])
+@connect((state, ownProps, actions) => ({
+    actions: {
+        setEventTickets: actions.core.setEventTickets,
+    },
+}))
+
+class Home extends PureComponent {
+
+    componentDidMount() {
+        const { actions, tickets } = this.props;
+
+        // adds the event tickets to redux, in case
+        // we need to use them somewhere else
+        actions.setEventTickets(tickets);
+    }
+
 
     render() {
+        const { tickets } = this.props;
         return (
             <div>
                 <BannerWithButton />
@@ -19,9 +51,9 @@ export default class Home extends PureComponent {
                 <RotatingCube />
                 <Banner />
                 <p styleName="last-event-label">חבילות או אירועים שמשתמשים צפו לאחרונה</p>
-                <EventCard />
-                <EventCard />
-                <EventCard />
+                {tickets.map((item) => (
+                    <EventCard event={item} />
+                ))}
             </div>
 
 
@@ -29,3 +61,4 @@ export default class Home extends PureComponent {
     }
 
 }
+export default Home;
