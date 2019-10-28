5881847b232576c47b9b5cb0db72eafeaac40a7c
ayah <ayah_kh5@hotmail.com>
2019-10-28 14:50:13 +0200

    change the banner component

--- src/modules/core/pages/Artist/index.jsx
+++ src/modules/core/pages/Artist/index.jsx
@@ -1,32 +1,31 @@
 import React, { PureComponent } from 'react';
 import Banner from '@/modules/common/components/Banner';
-import Slider from '@/modules/common/components/Slider';
 import EventsList from '@/modules/common/components/EventsList';
 import HotDestinations from '@/modules/common/components/HotDestinations';
 import EventDrop from '@/modules/common/components/EventDrop';
 import HotDrop from '@/modules/common/components/HotDrop';
-import Rectangle from '@/modules/common/components/Rectangle';
 import NavSlider from '@/modules/common/components/NavSlider';
 
 
 // import { withModal } from '@/modules/core/hoc/withModal';
 import './style.scss';
 
- export default class Artist extends PureComponent {
+export default class Artist extends PureComponent {
+
  state = {
-    display: false,
-    drop: false,
-  };
+     display: false,
+     drop: false,
+ };
 
 
    changeState= () => {
        if (this.state.display === false) {
-    this.setState({ display: true });
+           this.setState({ display: true });
        } else {
-      this.setState({ display: false });
-         }
+           this.setState({ display: false });
+       }
 
-}
+   }
 
    changeStateHot= () => {
        if (this.state.drop === false) {
@@ -40,12 +39,13 @@ import './style.scss';
     render() {
       console.log(this.props.modal);
         return (
-            <div styleName="background">
-                <Banner />
+           <div styleName="background">
+                <Banner title="מבצעים חמים" subtitle="הוא פשוט טקסט גולמי של תעשיית וההקלדה" img={require('@/assets/images/1_Anne-Marie.jpg')} />
 
                 <h1 styleName="title">אן מארי</h1>
                 <h3 styleName="subtitle">כולכם מוזמנים לקונצרטים</h3>
                   <h3 styleName="subtitle">באווירה קצת אחרת!</h3>
+
               <NavSlider />
 
                   <p styleName="describtion">היא השתלטה על כל פלייליסט אפשרי עם השיר .. בשנה שעברה וכעת מוכיחה לנו שהיא הרבה יותר אישה עם להיט אחד</p>
