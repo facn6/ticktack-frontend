a1b1566e77da279e8c9d9119495fb7ab3779b198
ayah <ayah_kh5@hotmail.com>
2019-10-28 11:51:30 +0200

    ff

--- src/modules/core/pages/Artist/index.jsx
+++ src/modules/core/pages/Artist/index.jsx
@@ -1,15 +1,23 @@
 import React, { PureComponent } from 'react';
 import Banner from '@/modules/common/components/Banner';
+<<<<<<< HEAD
 import EventsList from '@/modules/common/components/EventsList';
 import HotDestinations from '@/modules/common/components/HotDestinations';
 import EventDrop from '@/modules/common/components/EventDrop';
 import HotDrop from '@/modules/common/components/HotDrop';
 import NavSlider from '@/modules/common/components/NavSlider';
 
+=======
+import Slider from '@/modules/common/components/Slider';
+import EventsList from '@/modules/common/components/EventsList';
+import HotDestinations from '@/modules/common/components/HotDestinations';
+import Rectangle from '@/modules/common/components/Rectangle';
+>>>>>>> ff
 
 // import { withModal } from '@/modules/core/hoc/withModal';
 import './style.scss';
 
+<<<<<<< HEAD
 export default class Artist extends PureComponent {
 
  state = {
@@ -41,10 +49,19 @@ export default class Artist extends PureComponent {
         return (
            <div styleName="background">
                 <Banner title="מבצעים חמים" subtitle="הוא פשוט טקסט גולמי של תעשיית וההקלדה" img={require('@/assets/images/1_Anne-Marie.jpg')} />
+=======
+ export default class Artist extends PureComponent {
+    render() {
+      console.log(this.props.modal);
+        return (
+            <div styleName="background">
+                <Banner />
+>>>>>>> ff
 
                 <h1 styleName="title">אן מארי</h1>
                 <h3 styleName="subtitle">כולכם מוזמנים לקונצרטים</h3>
                   <h3 styleName="subtitle">באווירה קצת אחרת!</h3>
+<<<<<<< HEAD
 
               <NavSlider />
 
@@ -58,6 +75,18 @@ export default class Artist extends PureComponent {
                       {  this.state.display ? < EventDrop changeState={this.changeState} /> : null }
                   </div>
                   </div>
+=======
+              <Slider />
+                <p styleName="describtion">היא השתלטה על כל פלייליסט אפשרי עם השיר .. בשנה שעברה וכעת מוכיחה לנו שהיא הרבה יותר אישה עם להיט אחד</p>
+                  <HotDestinations />
+                <Rectangle location="אמסטרדם" time="7.03.2019"/>
+                  <div style={{  }}>
+                    <h1 styleName="title">כל האירועים של אן מארי</h1>
+                  <EventsList />
+
+                  </div>
+
+>>>>>>> ff
             </div>
         );
     }
